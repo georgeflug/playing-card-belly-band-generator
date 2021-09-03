42b161509eb57ded25596a36914841b8c314bd7e
Richard White <georgeflug@gmail.com>
2021-09-03 11:28:29 -0500

    Add default bands with sizes for ease of development

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,18 @@
 import { createStore } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import { BellyBandSpec } from '../belly-band'
 import { rootReducer } from './rootReduxReducer'
 
-export const store = createStore(rootReducer, { bellyBands: [{}] }, composeWithDevTools());
+const bellyBands: BellyBandSpec[] = [
+  {
+    width: 30,
+    height: 20,
+    depth: 10,
+  },
+  {
+    width: 35,
+    height: 25,
+    depth: 15,
+  }
+]
+export const store = createStore(rootReducer, { bellyBands }, composeWithDevTools());
