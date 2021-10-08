9033981898167e9355635f7df6ed6185fbf96ebe
Richard White <georgeflug@gmail.com>
2021-10-08 13:24:17 -0500

    Add types for new card enums

---
+++ src/card-configurations.ts
@@ -0,0 +1,4 @@
+export type CardOrientation = 'vertical' | 'horizontal'
+export type BandOrientation = 'vertical' | 'horizontal' | 'top'
+export type TextOrientation = 'normal' | 'rotated'
+export type BandSize = 'normal' | 'large' // perhaps I just draw all as extra long. I can draw 2 cut marks for those.
