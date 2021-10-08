9118af18080a3b560b8ec0369c7709f7b9e88f80
Richard White <georgeflug@gmail.com>
2021-10-08 15:21:11 -0500

    Add text to band tiles

--- src/form/BellyBandTile.tsx
+++ src/form/BellyBandTile.tsx
@@ -3,12 +3,21 @@ import { useEditBellyBand } from '../redux/editTileRedux'
 
 export const BellyBandTile = (props: { band: BellyBandSpec }) => {
   const { editBellyBand } = useEditBellyBand()
+  const { band } = props
 
   return (
-    <div style={{display: 'inline-block', border: '1px solid', height: '200px', width: '200px', cursor: 'pointer'}}
-      onClick={() => editBellyBand(props.band)}
+    <div style={{
+      display: 'inline-block',
+      border: '1px solid',
+      height: '200px',
+      lineHeight: '200px',
+      width: '200px',
+      cursor: 'pointer',
+      textAlign: 'center',
+    }}
+      onClick={() => editBellyBand(band)}
     >
-      Test
+        { band.text }
     </div>
   )
 }
