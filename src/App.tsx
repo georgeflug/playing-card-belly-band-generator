271499aa23bcc02cc4a0db522fd88fb56df991e4
Richard White <georgeflug@gmail.com>
2021-10-08 14:51:59 -0500

    Do not generate PDF until button is clicked

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { PDFViewer } from '@react-pdf/renderer'
+import { useState } from 'react'
 import { Provider } from 'react-redux'
 import './App.css';
 import { BellyBandEditor } from './form/BellyBandEditor'
@@ -8,6 +9,7 @@ import { store } from './redux/store'
 
 const App = () => {
   const {bellyBands} = useBellyBands();
+  const [ generated, setGenerated ] = useState(false)
 
   return (
     <div className="App">
@@ -15,12 +17,19 @@ const App = () => {
         <BellyBandEditor key={band.id} band={band} />
       )) }
       <div>
-        <PDFViewer width="100%" height="1024px">
-          <Provider store={store}>
-            <MyPdf />
-          </Provider>
-        </PDFViewer>
+        <button onClick={() => setGenerated(true)}>Generate</button>
       </div>
+      { generated && 
+        (
+          <div>
+            <PDFViewer width="100%" height="1024px">
+              <Provider store={store}>
+                <MyPdf />
+              </Provider>
+            </PDFViewer>
+          </div>
+        )
+      }
       {/* </header> */}
     </div>
   );
